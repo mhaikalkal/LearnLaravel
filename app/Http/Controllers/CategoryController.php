ddf1a51bdb75c5bf57e0c5cb97af1127a1431535
mhaikalkal <isalhaikal12@gmail.com>
2022-04-12 14:41:47 +0700

    added 'Eager Loading' and 'Lazy Eager Loading' to maximize performance

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,15 +11,16 @@ class CategoryController extends Controller
     public function categories() {
         return view('categories', [
             'title' => 'Post Categories',
+            'label' => 'All Categories',
             'categories' => Category::all(),
         ]);
     }
 
     public function PostsFromCategory(Category $category) {
-        return view('category', [
-            'title' => $category->name, // passing ke <title> nama tab-nya
-            'posts' => $category->posts, // ini kalo mau ambil banyak post dari sebuah category. karena post belongsTo category. Si posts ini merupakan public function posts() di model category
-            'category' => $category->name, // dipass sebagai label header.
+        return view('posts', [
+            'title' => $category->name, // passing ke <title> karena kita mengisinya menggunakan {{ $title }}
+            'label' => "Post(s) By Category : $category->name", // buat label di body
+            'posts' => $category->posts->load('category', 'author'), // ini kalo mau ambil banyak post dari sebuah category. karena post belongsTo category. Si posts ini merupakan public function posts() di model category
         ]);
     }
 
