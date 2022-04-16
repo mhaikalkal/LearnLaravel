17a861eea38cef61fa0557318fec7f79161ce0e5
mhaikalkal <isalhaikal12@gmail.com>
2022-04-16 11:06:45 +0700

    add custom gate with is_admin as reference

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        // gate, gerbang. artinya yang bisa mengakses hanya yang memenuhi syarat ini.
+        // cara make di yg lain.
+        // $this->authorize('admin')
+        // 'admin' sesuai nama yg di define dibawah ini.
+        Gate::define('admin', fn(User $user) => $user->is_admin);
     }
 }
